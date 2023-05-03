dbe92db86b8e3adb20986a6600ce037be4bd16ac
Raymond Zhu <raymond.zhu1999@gmail.com>
2023-05-03 22:03:32 +1000

    added suburb mapping

--- scrape_raywhite.py
+++ scrape_raywhite.py
@@ -7,7 +7,7 @@ header = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/5
 payload = "type=SAL&subtype=ANY&sort=&sortby=&status=CUR&baseUrl=&refpage=0&perpage=15&sortcombo=updated-DES---suburb&bedroom=any&bathroom=any&garage=any&event=&auctionmin=&auctionmax=&suburb=Homebush%2C+NSW+2140&radius=10&keywords=&agent=&office=&IF=&budgetmin=&budgetmax=&budgetdisplay=&landsizemin=&landsizemax=&floorsizemin=&floorsizemax=&comtype=&_s=buy&page=2"
 base_url = "https://www.raywhite.com/wp-admin/admin-ajax.php?action=dispatch&_p=rwcom/list/search/internal/true"
 pages_url = []  # list to store the url of every page
-listing_url = []  # list to store the url of every apartment 
+listing_url = []  # list to store the url of every apartment
 
 # begin get dom
 response = requests.request("POST", base_url, headers=header, data=payload)
